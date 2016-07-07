4af3d1cdd7621558e7032f417c7849063c261a0a
ebooij <ebooij@NB008038.bg.local>
2016-07-07 07:54:40 +0200

    removed unnecessary import

--- ibis-main/src/test/java/helpers/PageProvider.java
+++ ibis-main/src/test/java/helpers/PageProvider.java
@@ -2,7 +2,6 @@ package test.java.helpers;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
 
 import test.java.pageobjects.Ibis4RiskPage;
 import test.java.pageobjects.IbisMainPage;
